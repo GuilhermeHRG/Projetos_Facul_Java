71e7f7f89856b108b225ce9d2d2f44e252a9cdda
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-08-18 20:14:39 -0300

    tabuada.java

---
+++ tabuada.java
@@ -0,0 +1,17 @@
+package aula01;
+
+public class Main_exe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 10; i++){
+			System.out.println("TABUADA DO  "+i);
+			for(int j=1;j<11;j++) {
+				System.out.println(i+"*"+j+"="+i*j);
+				System.out.println();
+				}
+		}
+
+	}
+
+}
