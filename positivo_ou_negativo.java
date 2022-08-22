dc7db1aa3810f88b03923ccf0507716b01e77a58
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-08-22 20:53:39 -0300

    Update positivo_ou_negativo.java

--- positivo_ou_negativo.java
+++ positivo_ou_negativo.java
@@ -4,14 +4,16 @@ public class ex_03 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		
 		int n;
 		System.out.println("digite o valor de n: ");
 		n = ler.nextInt();
-		if (n>=0){
+		if (n>0){
 			System.out.println(n+" é postivo.");}
-		else{
+		else if (n<0){
 			System.out.println(n+" é negativo.");
+		}
+		else if (n==0){
+			System.out.println(n+" é um número nulo.");
 			ler.close();
 		}
 
