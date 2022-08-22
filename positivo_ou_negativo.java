9fac19013779b4c7edbc65aea4550afe833b8905
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-08-22 20:34:12 -0300

    positivo_ou_negativo
    Escreva um programa em Java para determinar se um dado número N (recebido através do teclado) é POSITIVO ou NEGATIVO.

---
+++ positivo_ou_negativo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex_03 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int n;
+		System.out.println("digite o valor de n: ");
+		n = ler.nextInt();
+		if (n>=0){
+			System.out.println(n+" é postivo.");}
+		else{
+			System.out.println(n+" é negativo.");
+		}
+
+	}
+
+}
