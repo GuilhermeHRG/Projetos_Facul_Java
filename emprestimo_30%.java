483381bc5507ab4c33f3006d3cbe25cd646d0b06
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-08-25 19:43:59 -0300

    Update emprestimo_30%.java

--- emprestimo_30%.java
+++ emprestimo_30%.java
@@ -5,11 +5,11 @@ public class ex11 {
 	public static void main(String[] args) {
 		
 		Scanner ler = new Scanner(System.in);
-		int sal, parc, sm;
+		float sal, parc, sm;
 		System.out.println("digite seu salario: ");
-		sal = ler.nextInt();
+		sal = ler.nextFloat();
 		System.out.println("digite o valor da prestação: ");
-		parc = ler.nextInt();
+		parc = ler.nextFloat();
 		sm = (sal/100)*30;
 		if (parc<=sm) {
 			System.out.println("empréstimo autorizado!!");
