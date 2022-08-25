ca31b4ddc8d4e2306d0da9ec52efa5fb0341f150
Guilherme Guelere <guilhermehenriqueescritorio@gmail.com>
2022-08-25 20:55:06 -0300

    Create meida.java
    Fazer um programa que imprima a média aritmética dos números 8,9 e 7. A média dos números 4, 5 e 6. A soma das duas médias. A média das médias.

---
+++ meida.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class EXEXEXE {
+
+	public static void main(String[] args) {
+				Scanner ler = new Scanner(System.in);
+				double v1, v2;
+				v1=(8.9+7)/2;
+				System.out.println(v1);
+				v2=(4+5+6)/3;
+				System.out.println(v2);
+				System.out.println((v1+v2)/2);
+	}
+
+}
